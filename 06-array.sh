378cfcf03e7a70e4cd040d099a7ba9bc96584cff
barath <bharathkumarkathi224@gmail.com>
2025-09-25 01:12:25 +0530

    variables

--- 06-array.sh
+++ 06-array.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #index starts from 0, size is 3
-FRUITS("APPLE" "KIWI" "ORANGE") #array
+FRUITS=("APPLE" "KIWI" "ORANGE") #array
 
 echo "first fruit is: ${FRUITS[0]}"
 echo "second fruit is: ${FRUITS[1]}"
