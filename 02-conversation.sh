7af1a88b9bf97cc34a3d5f463b9af5765e882d50
barath <bharathkumarkathi224@gmail.com>
2025-09-24 23:42:50 +0530

    variables

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -2,5 +2,5 @@
 
 echo "Ramesh:: Hi Suresh, How are you?"
 echo "Suresh:: Hello Ramesh, I am fine, How are you doing?"
-echo "Ranesh:: I am doing Suresh. What's going on?"
+echo "Ramesh:: I am doing Suresh. What's going on?"
 echo "Suresh:: I started learnig shell script Ramesh"
---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PERSON1=Ramesh
+PERSON2=Suresh
+
+
+echo "$PERSON1:: Hi ${PERSON2}, How are you?"
+echo "${PERSON2}:: Hello $PERSON1, I am fine, How are you doing?"
+echo "$PERSON1:: I am doing ${PERSON2}. What's going on?"
+echo "${PERSON2}:: I started learnig shell script $PERSON1"
