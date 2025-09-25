239d3cf19f20cb232a56f6c7c83d2410e429a182
barath <bharathkumarkathi224@gmail.com>
2025-09-25 13:48:04 +0530

    command

--- 12-functions.sh
+++ 12-functions.sh
@@ -1,45 +1,45 @@
 #!/bin/bash
 
 USERID=$(id -u)
-#echo "User id is: $USERID"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "Please run this script with root priveleges"
+        exit 1
+    fi
+}
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "Command is ..Failed"
-    else 
-        echo "Command is ..Success"
+        echo "$2 is...FAILED"
+        exit 1
+    else
+        echo "$2 is...SUCCESS"
     fi
-
 }
 
-if [ $USERID -ne 0 ]
-then
-    echo "Please run this script with root previleges"
-    exit 1
-fi
+CHECK_ROOT
 
-dnf list installed git 
-
-
-
+dnf list installed git
 
 if [ $? -ne 0 ]
-then 
-    echo "git is not installed,Going to install "
+then
+    echo "Git is not installed, going to install it.."
     dnf install git -y
-    VALIDATE $? "Installinng Git"
+    VALIDATE $? "Installing Git"
 else
-    echo "git is already installed, Nothing to do "
+    echo "Git is already installed, nothing to do.."
 fi
 
-dnf list install mysql
+dnf list installed mysql
 
 if [ $? -ne 0 ]
 then
-    echo "MYSQL is not installed ,Going to install"
+    echo "MySQL is not installed...going to install"
     dnf install mysql -y
-    VALIDATE $? "Installing MYSQL"
+    VALIDATE $? "Installing MySQL"
 else
-    echo "MYSQL is already installed, nothing to do "
+    echo "MySQL is already installed..nothing to do"
 fi
