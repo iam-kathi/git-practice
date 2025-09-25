b523402401858659d9a21be354cecbe30137862d
barath <bharathkumarkathi224@gmail.com>
2025-09-25 09:56:39 +0530

    command

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -7,3 +7,5 @@ echo "Script Name: $0"
 echo "Current working directory is: $PWD"
 echo "Home directory of current user is: $HOME"
 echo "PID of the script executing now: $$"
+sleep 100 &
+echo "PID of last background command: $!"
